d7e11c3c07991f245ee346d97d611b22b4d58695
tvisenti <tvisenti@student.42.fr>
2017-05-26 17:54:07 +0200

    Try to add incoming sms

---
+++ Ft_Hangouts/app/src/main/java/com/tvisenti/ft_hangouts/IncomingSms.java
@@ -0,0 +1,50 @@
+package com.tvisenti.ft_hangouts;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.telephony.SmsMessage;
+import android.util.Log;
+import android.widget.Toast;
+
+/**
+ * Created by tvisenti on 5/26/17.
+ */
+
+public class IncomingSms extends BroadcastReceiver {
+
+    public void onReceive(Context context, Intent intent)
+    {
+        Bundle myBundle = intent.getExtras();
+        SmsMessage [] messages = null;
+        String strMessage = "";
+
+        if (myBundle != null)
+        {
+            Object [] pdus = (Object[]) myBundle.get("pdus");
+
+            messages = new SmsMessage[pdus.length];
+
+            for (int i = 0; i < messages.length; i++)
+            {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+                    String format = myBundle.getString("format");
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i], format);
+                }
+                else {
+                    messages[i] = SmsMessage.createFromPdu((byte[]) pdus[i]);
+                }
+                strMessage += "SMS From: " + messages[i].getOriginatingAddress();
+                strMessage += " : ";
+                strMessage += messages[i].getMessageBody();
+                strMessage += "\n";
+            }
+
+            Log.e("SMS", strMessage);
+            Toast.makeText(context, strMessage, Toast.LENGTH_SHORT).show();
+        }
+    }
+}
